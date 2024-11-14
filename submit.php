193fab3f260701bd73a57879d658b569d455ffc3
mrfaizan123 <umohdfaizan@gmail.com>
2024-11-14 11:52:39 +0530

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,1 +1,31 @@
-VYWKPSS26K2PRLVNGDBR6K3M
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Wedding_collection";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $Place = $_POST['Place'];
+    $Amount = $_POST['Amount'];
+    
+    $stmt_insert = $conn->prepare("INSERT INTO your_table_name (name, place, amount) VALUES (?, ?, ?)");
+    $stmt_insert->bind_param("sss", $name, $Place, $Amount);
+
+    if ($stmt_insert->execute()) {
+        echo "New member added successfully.";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+
+    $stmt_insert->close();
+}
+
+$conn->close();
+?>
